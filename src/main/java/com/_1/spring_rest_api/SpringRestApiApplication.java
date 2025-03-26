e2d4c73b71d924bc8a655decc4b795b7fba15919
Joon9750 <junhyeok2166@daum.net>
2025-03-26 16:48:45 +0900

    [Add] 패키지 구조 설정

--- src/main/java/com/_1/spring_rest_api/SpringRestApiApplication.java
+++ src/main/java/com/_1/spring_rest_api/SpringRestApiApplication.java
@@ -2,7 +2,9 @@ package com._1.spring_rest_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SpringRestApiApplication {
 
