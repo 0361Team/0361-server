e71f13933009e68d20b2f3c5d147cb0d041214b7
Dmori <83327857+Dimo-2562@users.noreply.github.com>
2025-04-06 16:24:07 +0900

    [Feature] 테스트 전용 SecurityConfig 클래스 추가 및 환경설정 (#13)

---
+++ src/main/java/com/_1/spring_rest_api/config/TestSecurityConfig.java
@@ -0,0 +1,26 @@
+package com._1.spring_rest_api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@Profile("test") //테스트 프로필에서만 활성화
+public class TestSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll() // 모든 요청 허용
+                )
+                .csrf(csrf -> csrf.disable()) // CSRF 보호 비활성화
+                .cors(cors -> cors.disable()); // CORS 비활성화
+
+        return http.build();
+    }
+}
