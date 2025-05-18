0a1c679b0da61de751b16d1d27e5f07452781a29
dmori <chosehoon2@naver.com>
2025-05-18 17:24:10 +0900

    [Fix] Swagger에서 https 사용가능하도록 변경

--- src/main/java/com/_1/spring_rest_api/config/SwaggerConfig.java
+++ src/main/java/com/_1/spring_rest_api/config/SwaggerConfig.java
@@ -3,17 +3,36 @@ package com._1.spring_rest_api.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration    // 스프링 실행시 설정파일 읽어드리기 위한 어노테이션
 public class SwaggerConfig {
 
+    @Value("${server.api.url}")
+    private String serverApiUrl;
+
     @Bean
     public OpenAPI openAPI() {
+        // JWT 보안 스키마 정의
+        SecurityScheme jwtScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
         return new OpenAPI()
-                .components(new Components())
-                .info(apiInfo());
+                .components(new Components().addSecuritySchemes("JWT", jwtScheme))
+                .addSecurityItem(new SecurityRequirement().addList("JWT"))
+                .info(apiInfo())
+                .servers(List.of(new Server().url(serverApiUrl)));  // 환경 변수에서 가져온 URL 사용
     }
 
     private Info apiInfo() {
