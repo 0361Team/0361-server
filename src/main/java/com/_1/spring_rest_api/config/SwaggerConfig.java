855cb00b1889272110cd1fa2c7f1160cff08d12b
dmori <chosehoon2@naver.com>
2025-05-18 16:47:25 +0900

    [Fix] https/http 혼합 문제 해결

--- src/main/java/com/_1/spring_rest_api/config/SwaggerConfig.java
+++ src/main/java/com/_1/spring_rest_api/config/SwaggerConfig.java
@@ -3,17 +3,35 @@ package com._1.spring_rest_api.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration    // 스프링 실행시 설정파일 읽어드리기 위한 어노테이션
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
+        // JWT 보안 스키마 정의
+        SecurityScheme jwtScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
+        // 보안 요구사항 설정
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("JWT");
+
         return new OpenAPI()
-                .components(new Components())
-                .info(apiInfo());
+                .components(new Components().addSecuritySchemes("JWT", jwtScheme))
+                .addSecurityItem(securityRequirement)
+                .info(apiInfo())
+                .servers(List.of(new Server().url("https://spring-container-620597935007.us-central1.run.app"))); // HTTPS 명시
     }
 
     private Info apiInfo() {
