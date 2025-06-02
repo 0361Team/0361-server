d09b71107c031e02ac1b4d6503465483278bda8b
dmori <chosehoon2@naver.com>
2025-06-02 19:17:27 +0900

    feat: 배치 세션 삭제 비즈니스 로직 구현

--- src/main/java/com/_1/spring_rest_api/service/QuizSessionCommandService.java
+++ src/main/java/com/_1/spring_rest_api/service/QuizSessionCommandService.java
@@ -2,10 +2,14 @@ package com._1.spring_rest_api.service;
 
 import com._1.spring_rest_api.api.dto.AnswerRequest;
 import com._1.spring_rest_api.api.dto.AnswerResponse;
+import com._1.spring_rest_api.api.dto.DeleteSessionsRequest;
+import com._1.spring_rest_api.api.dto.DeleteSessionsResponse;
 
 public interface QuizSessionCommandService {
 
     AnswerResponse answerQuestion(Long sessionId, AnswerRequest request);
 
     void completeSession(Long sessionId);
+
+    DeleteSessionsResponse deleteSessions(DeleteSessionsRequest request);
 }
